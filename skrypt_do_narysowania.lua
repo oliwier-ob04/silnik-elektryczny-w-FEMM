0c2f5ab5c398479434905adec0c6893a5227e72d
Oliwier <oliwier.ob04+git.hub@gmail.com>
2026-06-16 09:09:05 +0200

    Gotowy skrypt_do_narysowania.lua

--- skrypt_do_narysowania.lua
+++ skrypt_do_narysowania.lua
@@ -1,8 +1,13 @@
+-- parametry pracy: prad, liczba zwojow, kat wirnika
 current = 7
-turns = 1
+turns = 100
 rotor_angle = 0
 pi = 3.14159265358979
 
+-- grupa wirnika (czesc obracana)
+ROTOR = 1
+
+-- promienie kolejnych warstw silnika [mm] (od otworu po obszar zewnetrzny)
 R1 = 20
 R2 = 30
 R3 = 50
@@ -12,17 +17,20 @@ R6 = 63
 R7 = 66
 R8 = 78
 
+-- liczba zlobkow i magnesow oraz ich ustawienie katowe
 Ns = 12
 Nr = 14
 theta0_s = 15
 theta0_r = 90
 
+-- ksztalt (szerokosci katowe) zlobkow stojana i magnesow
 delta_inner = 4 * pi / 180
 delta_outer = 7.5 * pi / 180
 delta_mid   = 3 * pi / 180
 delta_pink_inner = 2 * pi / 180
 delta_pink_outer = 3.5 * pi / 180
 
+-- funkcja pomocnicza: okrag
 function draw_circle(R)
     mi_addnode(R,0)
     mi_addnode(-R,0)
@@ -30,9 +38,11 @@ function draw_circle(R)
     mi_addarc(-R,0,R,0,180,1)
 end
 
+-- definicja zadania symulacji (pole magnetyczne 2D, mm, glebokosc 50 mm)
 newdocument(0)
-mi_probdef(0,"millimeters","planar",1e-8,0,30)
+mi_probdef(0,"millimeters","planar",1e-8,50,30)
 
+-- okregi rozdzielajace warstwy silnika
 draw_circle(R1)
 draw_circle(R2)
 draw_circle(R3)
@@ -42,6 +52,7 @@ draw_circle(R6)
 draw_circle(R7)
 draw_circle(R8)
 
+-- zlobki stojana - linie promieniowe
 for n = 0, Ns-1 do
     local theta = (theta0_s + n*(360/Ns)) * pi / 180
     local x1 = R2 * cos(theta)
@@ -53,6 +64,7 @@ for n = 0, Ns-1 do
     mi_addsegment(x1,y1,x2,y2)
 end
 
+-- zlobki stojana - scianki i wejscia
 for n = 0, Ns-1 do
     local theta = (theta0_s + n*(360/Ns)) * pi / 180
     for side = -1, 1, 2 do
@@ -73,6 +85,7 @@ for n = 0, Ns-1 do
     end
 end
 
+-- magnesy wirnika - scianki (podzial wienca)
 for n = 0, Nr-1 do
     local theta = (theta0_r + n*(360/Nr)) * pi / 180
     for side = -1, 1, 2 do
@@ -86,6 +99,7 @@ for n = 0, Nr-1 do
     end
 end
 
+-- formowanie zlobkow i rozdzielenie magnesow
 for n = 0, Ns-1 do
     local theta = (theta0_s + (n + 0.5)*(360/Ns)) * pi / 180
     mi_selectarcsegment(R2 * cos(theta), R2 * sin(theta))
@@ -117,55 +131,60 @@ mi_selectnode(R3,0)
 mi_selectnode(-R3,0)
 mi_deleteselected()
 
+-- materialy silnika z biblioteki
 mi_getmaterial("Air")
 mi_getmaterial("M-19 Steel")
 mi_getmaterial("N40")
 mi_getmaterial("20 AWG")
 
+-- obwody trzech faz uzwojen
 mi_addcircprop("A", current, 1)
 mi_addcircprop("B", current, 1)
 mi_addcircprop("C", current, 1)
 
+-- warunek brzegowy na zewnetrznej krawedzi
 mi_addboundprop("Dirichlet", 0, 0, 0, 0, 0, 0, 0, 0, 0)
 mi_selectarcsegment(0, R8)
 mi_setarcsegmentprop(5, "Dirichlet", 0, 0)
 mi_clearselected()
 
+-- otwor srodkowy (wal) - powietrze
 mi_addblocklabel(0, R1 * 0.5)
 mi_selectlabel(0, R1 * 0.5)
 mi_setblockprop("Air", 1, 1, "", 0, 0, 0)
 mi_clearselected()
 
+-- jarzmo i zeby stojana - stal
 mi_addblocklabel((R1 + R2) * 0.5, 0)
 mi_selectlabel((R1 + R2) * 0.5, 0)
 mi_setblockprop("M-19 Steel", 1, 1, "", 0, 0, 0)
 mi_clearselected()
 
-mi_addblocklabel((R3 + R4) * 0.5, 0)
-mi_selectlabel((R3 + R4) * 0.5, 0)
-mi_setblockprop("M-19 Steel", 1, 1, "", 0, 0, 0)
-mi_clearselected()
-
+-- szczelina powietrzna
 mi_addblocklabel((R4 + R5) * 0.5, 0)
 mi_selectlabel((R4 + R5) * 0.5, 0)
 mi_setblockprop("Air", 1, 0.5, "", 0, 0, 0)
 mi_clearselected()
 
+-- jarzmo wirnika - stal (obraca sie z magnesami)
 mi_addblocklabel((R6 + R7) * 0.5, 0)
 mi_selectlabel((R6 + R7) * 0.5, 0)
-mi_setblockprop("Air", 1, 0.5, "", 0, 0, 0)
+mi_setblockprop("M-19 Steel", 1, 0.5, "", 0, 0, 0)
 mi_clearselected()
 
+-- obszar zewnetrzny - powietrze
 mi_addblocklabel((R7 + R8) * 0.5, 0)
 mi_selectlabel((R7 + R8) * 0.5, 0)
 mi_setblockprop("Air", 1, 1, "", 0, 0, 0)
 mi_clearselected()
 
+-- przypisanie faz (A/B/C) i kierunkow pradu do zlobkow
 phase_A = {"A","A","B","B","C","C","A","A","B","B","C","C",
            "A","A","B","B","C","C","A","A","B","B","C","C"}
 dir_A   = { 1,  1,  1,  1,  1,  1, -1, -1, -1, -1, -1, -1,
              1,  1,  1,  1,  1,  1, -1, -1, -1, -1, -1, -1}
 
+-- uzwojenia w zlobkach stojana
 r_winding = (R2 + R3) * 0.5
 slot_mid = (delta_inner + delta_outer) * 0.5
 idx = 1
@@ -177,12 +196,13 @@ for n = 0, Ns-1 do
         local py = r_winding * sin(theta)
         mi_addblocklabel(px, py)
         mi_selectlabel(px, py)
-        mi_setblockprop("20 AWG", 0, 0.5, phase_A[idx], 0, dir_A[idx] * turns, 0)
+        mi_setblockprop("20 AWG", 0, 0.5, phase_A[idx], 0, 0, dir_A[idx] * turns)
         mi_clearselected()
         idx = idx + 1
     end
 end
 
+-- magnesy trwale wirnika (namagnesowanie naprzemienne N-S)
 r_magnet = (R5 + R6) * 0.5
 for i = 0, Nr-1 do
     local base_deg = theta0_r + i * (360 / Nr)
@@ -201,4 +221,21 @@ for i = 0, Nr-1 do
     mi_clearselected()
 end
 
+-- wirnik jako grupa (magnesy + jarzmo) do obracania
+mi_clearselected()
+mi_selectcircle(0, 0, R7 + 1, 4)
+mi_setgroup(ROTOR)
+mi_clearselected()
+mi_selectcircle(0, 0, R5 - 0.5, 4)
+mi_setgroup(0)
+mi_clearselected()
+
+-- ustawienie wirnika pod zadanym katem
+if rotor_angle ~= 0 then
+    mi_selectgroup(ROTOR)
+    mi_moverotate(0, 0, rotor_angle, 4)
+    mi_clearselected()
+end
+
+-- widok
 mi_zoomnatural()
