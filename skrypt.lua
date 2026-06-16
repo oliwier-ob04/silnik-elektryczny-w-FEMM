0fdd2c144957f7053758749d6d9f00fd7dc73e21
Oliwier <oliwier.ob04+git.hub@gmail.com>
2026-06-16 09:05:30 +0200

    Ujednolicono skrypt

--- skrypt.lua
+++ skrypt.lua
@@ -1,3 +1,8 @@
+current = 7
+turns = 1
+rotor_angle = 0
+pi = 3.14159265358979
+
 R1 = 20
 R2 = 30
 R3 = 50
@@ -6,6 +11,17 @@ R5 = 58
 R6 = 63
 R7 = 66
 R8 = 78
+
+Ns = 12
+Nr = 14
+theta0_s = 15
+theta0_r = 90
+
+delta_inner = 4 * pi / 180
+delta_outer = 7.5 * pi / 180
+delta_mid   = 3 * pi / 180
+delta_pink_inner = 2 * pi / 180
+delta_pink_outer = 3.5 * pi / 180
 
 function draw_circle(R)
     mi_addnode(R,0)
@@ -26,29 +42,12 @@ draw_circle(R6)
 draw_circle(R7)
 draw_circle(R8)
 
-Ns = 12
-Nr = 14
-
-theta0_s = 15
-theta0_r = 90
-
-pi = 3.14159265358979
-
-delta_inner = 4 * pi / 180
-delta_outer = 7.5 * pi / 180
-delta_mid   = 3 * pi / 180
-
-delta_pink_inner = 2 * pi / 180
-delta_pink_outer = 3.5 * pi / 180
-
 for n = 0, Ns-1 do
     local theta = (theta0_s + n*(360/Ns)) * pi / 180
-
     local x1 = R2 * cos(theta)
     local y1 = R2 * sin(theta)
     local x2 = R3 * cos(theta)
     local y2 = R3 * sin(theta)
-
     mi_addnode(x1,y1)
     mi_addnode(x2,y2)
     mi_addsegment(x1,y1,x2,y2)
@@ -56,23 +55,18 @@ end
 
 for n = 0, Ns-1 do
     local theta = (theta0_s + n*(360/Ns)) * pi / 180
-
     for side = -1, 1, 2 do
-
         local x1 = R2 * cos(theta + side*delta_inner)
         local y1 = R2 * sin(theta + side*delta_inner)
         local x2 = R3 * cos(theta + side*delta_outer)
         local y2 = R3 * sin(theta + side*delta_outer)
-
         mi_addnode(x1,y1)
         mi_addnode(x2,y2)
         mi_addsegment(x1,y1,x2,y2)
-
         local xm1 = R3 * cos(theta + side*delta_mid)
         local ym1 = R3 * sin(theta + side*delta_mid)
         local xm2 = R4 * cos(theta + side*delta_mid)
         local ym2 = R4 * sin(theta + side*delta_mid)
-
         mi_addnode(xm1,ym1)
         mi_addnode(xm2,ym2)
         mi_addsegment(xm1,ym1,xm2,ym2)
@@ -81,15 +75,11 @@ end
 
 for n = 0, Nr-1 do
     local theta = (theta0_r + n*(360/Nr)) * pi / 180
-
     for side = -1, 1, 2 do
-
         local x1 = R5 * cos(theta + side*delta_pink_inner)
         local y1 = R5 * sin(theta + side*delta_pink_inner)
-
         local x2 = R6 * cos(theta + side*delta_pink_outer)
         local y2 = R6 * sin(theta + side*delta_pink_outer)
-
         mi_addnode(x1,y1)
         mi_addnode(x2,y2)
         mi_addsegment(x1,y1,x2,y2)
@@ -98,54 +88,117 @@ end
 
 for n = 0, Ns-1 do
     local theta = (theta0_s + (n + 0.5)*(360/Ns)) * pi / 180
-
-    local x = R2 * cos(theta)
-    local y = R2 * sin(theta)
-
-    mi_selectarcsegment(x,y)
+    mi_selectarcsegment(R2 * cos(theta), R2 * sin(theta))
 end
-
 mi_deleteselected()
 
 for n = 0, Ns-1 do
     local theta = (theta0_s + (n + 0.5)*(360/Ns)) * pi / 180
-
-    local x = R3 * cos(theta)
-    local y = R3 * sin(theta)
-
-    mi_selectarcsegment(x,y)
+    mi_selectarcsegment(R3 * cos(theta), R3 * sin(theta))
 end
-
 mi_deleteselected()
 
 for n = 0, Ns-1 do
     local theta = (theta0_s + n*(360/Ns)) * pi / 180
-
-    local x = R4 * cos(theta)
-    local y = R4 * sin(theta)
-
-    mi_selectarcsegment(x,y)
+    mi_selectarcsegment(R4 * cos(theta), R4 * sin(theta))
 end
-
 mi_deleteselected()
 
 for n = 0, Nr-1 do
     local theta = (theta0_r + n*(360/Nr)) * pi / 180
-
-    local x = R5 * cos(theta)
-    local y = R5 * sin(theta)
-
-    mi_selectarcsegment(x,y)
+    mi_selectarcsegment(R5 * cos(theta), R5 * sin(theta))
 end
-
 mi_deleteselected()
 
 mi_selectnode(R2,0)
 mi_selectnode(-R2,0)
 mi_deleteselected()
-
 mi_selectnode(R3,0)
 mi_selectnode(-R3,0)
 mi_deleteselected()
+
+mi_getmaterial("Air")
+mi_getmaterial("M-19 Steel")
+mi_getmaterial("N40")
+mi_getmaterial("20 AWG")
+
+mi_addcircprop("A", current, 1)
+mi_addcircprop("B", current, 1)
+mi_addcircprop("C", current, 1)
+
+mi_addboundprop("Dirichlet", 0, 0, 0, 0, 0, 0, 0, 0, 0)
+mi_selectarcsegment(0, R8)
+mi_setarcsegmentprop(5, "Dirichlet", 0, 0)
+mi_clearselected()
+
+mi_addblocklabel(0, R1 * 0.5)
+mi_selectlabel(0, R1 * 0.5)
+mi_setblockprop("Air", 1, 1, "", 0, 0, 0)
+mi_clearselected()
+
+mi_addblocklabel((R1 + R2) * 0.5, 0)
+mi_selectlabel((R1 + R2) * 0.5, 0)
+mi_setblockprop("M-19 Steel", 1, 1, "", 0, 0, 0)
+mi_clearselected()
+
+mi_addblocklabel((R3 + R4) * 0.5, 0)
+mi_selectlabel((R3 + R4) * 0.5, 0)
+mi_setblockprop("M-19 Steel", 1, 1, "", 0, 0, 0)
+mi_clearselected()
+
+mi_addblocklabel((R4 + R5) * 0.5, 0)
+mi_selectlabel((R4 + R5) * 0.5, 0)
+mi_setblockprop("Air", 1, 0.5, "", 0, 0, 0)
+mi_clearselected()
+
+mi_addblocklabel((R6 + R7) * 0.5, 0)
+mi_selectlabel((R6 + R7) * 0.5, 0)
+mi_setblockprop("Air", 1, 0.5, "", 0, 0, 0)
+mi_clearselected()
+
+mi_addblocklabel((R7 + R8) * 0.5, 0)
+mi_selectlabel((R7 + R8) * 0.5, 0)
+mi_setblockprop("Air", 1, 1, "", 0, 0, 0)
+mi_clearselected()
+
+phase_A = {"A","A","B","B","C","C","A","A","B","B","C","C",
+           "A","A","B","B","C","C","A","A","B","B","C","C"}
+dir_A   = { 1,  1,  1,  1,  1,  1, -1, -1, -1, -1, -1, -1,
+             1,  1,  1,  1,  1,  1, -1, -1, -1, -1, -1, -1}
+
+r_winding = (R2 + R3) * 0.5
+slot_mid = (delta_inner + delta_outer) * 0.5
+idx = 1
+for n = 0, Ns-1 do
+    local theta_c = (theta0_s + n*(360/Ns)) * pi / 180
+    for s = -1, 1, 2 do
+        local theta = theta_c + s * slot_mid
+        local px = r_winding * cos(theta)
+        local py = r_winding * sin(theta)
+        mi_addblocklabel(px, py)
+        mi_selectlabel(px, py)
+        mi_setblockprop("20 AWG", 0, 0.5, phase_A[idx], 0, dir_A[idx] * turns, 0)
+        mi_clearselected()
+        idx = idx + 1
+    end
+end
+
+r_magnet = (R5 + R6) * 0.5
+for i = 0, Nr-1 do
+    local base_deg = theta0_r + i * (360 / Nr)
+    local angle_rad = (base_deg + 8) * pi / 180
+    local px = r_magnet * cos(angle_rad)
+    local py = r_magnet * sin(angle_rad)
+    local mag_angle
+    if (i - 2 * floor(i / 2)) == 0 then
+        mag_angle = base_deg
+    else
+        mag_angle = base_deg + 180
+    end
+    mi_addblocklabel(px, py)
+    mi_selectlabel(px, py)
+    mi_setblockprop("N40", 1, 1, "", mag_angle, 0, 0)
+    mi_clearselected()
+end
 
 mi_zoomnatural()
